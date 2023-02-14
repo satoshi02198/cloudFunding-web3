58ece41928991f33b8b6e517a265148186a50bdd
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-02-14 13:30:15 +0900

    4

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
-import Campaign from "../ethereum/Campaign";
+import Campaign from "../ethereum/campaign";
 import web3 from "../ethereum/web3";
 import { Router } from "../routes";
 
